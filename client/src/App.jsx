38d5fe8e9ea8b2692f80799d0346366212bfdea7
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-26 22:41:54 +0530

    configured tailwind css for frontend and tested

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
+import './App.css'
 function App() {
 
   return (
-    <>
-     <h1>Hello World</h1>
-    </>
+
+    <h1 class="text-4xl font-bold text-center text-blue-600 my-4 p-2 shadow-lg tracking-wider">
+    Hello, World!
+  </h1>
+  
+
+
   )
 }
 
