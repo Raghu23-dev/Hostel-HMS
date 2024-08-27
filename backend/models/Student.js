e05af395006bc05e92bdbb631c81be1c32e1cfb5
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-27 13:07:02 +0530

    created model/Schema for backend

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -2,52 +2,65 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const StudentSchema = new Schema({
-    name: {
-        type: String,
-        required: true
+    name:{
+        type:String,
+        required:true
     },
-    aadhar_no: {  
-        type: Number,
-        required: true,
-        unique: true
+    cms_id:{
+        type:Number,
+        required:true,
+        unique:true
     },
-    room_no: {
-        type: Number,
-        required: true
+    room_no:{
+        type:Number,
+        required:true
     },
-    batch: {
-        type: Number,
-        required: true
+    batch:{
+        type:Number,
+        required:true
     },
-    dept: {
-        type: String,
-        required: true
+    dept:{
+        type:String,
+        required:true
     },
-    course: {
-        type: String,
-        required: true
+    course:{
+        type:String,
+        required:true
     },
-    email: {
-        type: String,
-        required: true
+    email:{
+        type:String,
+        required:true,
+        unique:true
     },
-    contact: {
-        type: String,
-        required: true
+    father_name:{
+        type:String,
+        required:true
     },
-    password: {
-        type: String,
-        required: true
+    contact:{
+        type:String,
+        required:true
     },
-    date: {
-        type: Date,
-        default: Date.now
+    address:{
+        type:String,
+        required:true
+    },
+    dob:{
+        type:Date,
+        required:true
+    },
+    cnic:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    user:{
+        type:Schema.Types.ObjectId,
+        ref:'user'
+    },
+    hostel:{
+        type:Schema.Types.ObjectId,
+        ref:'hostel'
     }
-});
+})
 
-StudentSchema.virtual('primaryKey').get(function(){
-    return this.aadhar_no;  
-});
-
-const Student = mongoose.model('student', StudentSchema);
-module.exports = Student;
+module.exports = Student = mongoose.model('student',StudentSchema);
