48d075a9e39b197476dc2b4c9615502fa096886b
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-28 01:30:13 +0530

    fixed Student Home in Topbar

--- client/src/components/Dashboards/Studentdashboard/Home.jsx
+++ client/src/components/Dashboards/Studentdashboard/Home.jsx
@@ -1,13 +1,18 @@
-// import { Doughnut } from "react-chartjs-2";
-// import 'chart.js/auto'; 
 
 function Home() {
+  let user = JSON.parse(localStorage.getItem("user"));
+
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <h1 className="text-white font-bold text-5xl">
-        Welcome <span className="text-blue-700">Student!</span>
-      </h1>
-    </div>
+    <>
+      <div className="w-full h-screen flex items-center justify-center">
+        <h1 className="text-white font-bold text-5xl">
+          Welcome <span className="text-blue-700">{user.email}</span>
+        </h1>
+      </div>
+      <div className="w-full h-screen flex items-center justify-center" >
+          <h1 className="text-gray-900 font-bold text-5xl" >Welcome <span className="text-blue-500">{user.email}</span></h1>
+      </div>
+    </>
   );
 }
 
