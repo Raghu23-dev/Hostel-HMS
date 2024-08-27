c6484f89ade646978048ceb548452b970bb2f4be
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-27 14:42:57 +0530

    student controllers for admin routes

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json({ extended: false }));
 
 app.use('/api/auth', require('./routes/authRoutes'));
+app.use('/api/admin', require('./routes/adminRoutes'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
