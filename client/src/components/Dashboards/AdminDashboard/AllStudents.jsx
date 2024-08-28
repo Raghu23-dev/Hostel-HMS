6f98c688072d7c037885b77d6b9ac75ee2a17d21
raghu23-dev <raghu2308.dev@gmail.com>
2024-08-28 16:07:08 +0530

    resolved complaints, attendance, all student page

---
+++ client/src/components/Dashboards/AdminDashboard/AllStudents.jsx
@@ -0,0 +1,106 @@
+import { useState } from "react";
+function AllStudents() {
+  const [allStudents, setAllStudents] = useState([
+    {
+      name: "AbdulAhad Qureshi",
+      cms: "368115",
+      room: 321,
+      hostel: "Attar-1",
+    },
+    {
+      name: "Danish Azeem",
+      cms: "370421",
+      room: 327,
+      hostel: "Attar-2",
+    },
+  ]);
+
+  return (
+    <div className="w-full h-screen flex flex-col gap-5 items-center justify-center">
+      <h1 className="text-white font-bold text-5xl">All Students</h1>
+      <div className="w-96 flex justify-center">
+        <a
+          href="../public/paging.pdf"
+          target="_blank"
+          download={true}
+          className="px-20 py-3 bg-blue-700 hover:bg-blue-800 text-white font-bold rounded-xl shadow-xl"
+        >
+          Download List
+        </a>
+      </div>
+      <div className="bg-neutral-950 px-10 py-5 rounded-xl shadow-xl sm:w-96 w-full mt-5 max-h-96 overflow-auto">
+        <span className="text-white font-bold text-xl">All Students</span>
+        <ul role="list" className="divide-y divide-gray-700 text-white">
+          {allStudents.length === 0
+            ? "No new complaints!"
+            : allStudents.map((student) => (
+                <li className="py-3 sm:py-4" key={student.cms}>
+                  <div className="flex items-center space-x-4">
+                    <div className="flex-shrink-0 text-white">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={2}
+                        stroke="currentColor"
+                        className="w-7 h-7"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M12 9v3.75m9-.75a9 9 0 11-18 0 9 9 0 0118 0zm-9 3.75h.008v.008H12v-.008z"
+                        />
+                      </svg>
+                    </div>
+                    <div className="flex-1 min-w-0">
+                      <p className="text-sm font-medium truncate text-white">
+                        {student.name}
+                      </p>
+                      <p className="text-sm truncate text-gray-400">
+                        {student.cms} | Room: {student.room}
+                      </p>
+                    </div>
+                    <div className="flex gap-3">
+                      <button className="hover:underline">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                          />
+                        </svg>
+                      </button>
+                      <button className="hover:underline ">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          strokeWidth={1.5}
+                          stroke="currentColor"
+                          className="w-6 h-6"
+                        >
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+                          />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                </li>
+              ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default AllStudents;
